d30744dfd3453fc1edae9aea8691cb7a8ce3f3ac
Didar Yermekuly <yermekulyd@gmail.com>
2024-10-01 18:14:21 +0500

    global: fetching data through axios, displaying it as a menu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
 import { Menu } from "antd";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const items = [
-  {
-    key: "g1",
-    label: "Список криптовалют",
-    type: "group",
-    children: [
-      {
-        key: "1",
-        label: "Option 1",
-      },
-      {
-        key: "2",
-        label: "Option 2",
-      },
-    ],
-  },
-];
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
 
 const App = () => {
+  const [currencies, setCurrencies] = useState([]);
+
+  const fetchCurrencies = () => {
+    axios.get("http://127.0.0.1:8000/cryptocurrencies").then((r) => {
+      const currenciesResponse = r.data;
+      const menuItems = [
+        getItem(
+          "Список криптовалют",
+          "g1",
+          null,
+          currenciesResponse.map((c) => {
+            return { label: c.name, key: c.id };
+          }),
+          "group"
+        ),
+      ];
+      setCurrencies(menuItems);
+    });
+  };
+
+  useEffect(() => {
+    fetchCurrencies();
+  }, []);
+
   const onClick = (e) => {
     console.log("click ", e);
   };
@@ -31,7 +49,7 @@ const App = () => {
       defaultSelectedKeys={["1"]}
       defaultOpenKeys={["sub1"]}
       mode="inline"
-      items={items}
+      items={currencies}
     />
   );
 };
